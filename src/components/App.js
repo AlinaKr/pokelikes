a7c0071edd0bb6387a038e192b76a4144051d0d6
Maxence Bouret <maxence.bouret@gmail.com>
2018-09-11 16:27:14 +0200

    Add of 2 columns of mv of pictures

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,47 @@ import React, { Component } from 'react';
 import PokeDetail from './PokeDetail';
 import './App.css';
 
+import pokemons from '../data/pokemons'
+
+console.log("pokemons", pokemons)
+
 class App extends Component {
+  getPokemonsList() {
+    let pokemonsList = []
+    for (let i = 0; i < pokemons.length; i++) {
+      pokemonsList.push(
+        <div className="card">{pokemons[i].name}</div>
+      )
+    }
+    return pokemonsList
+  }
   render() {
+    let pokemonsList = []
+    for (let i = 0; i < pokemons.length; i++) {
+      pokemonsList.push(
+        <div className="card">{pokemons[i].name}</div>
+      )
+    }
     return (
       <div className="App">
         <h1>PokeLikes</h1>
-        <PokeDetail />
+        <div className="columns">
+          <div class="column is-5 list-pokemon">
+            {pokemons.map(pokemon => (
+              <div
+                className="card"
+                key={pokemon.national_id}
+                onClick={e => console.log(pokemon.name)}>
+                <img src={`/pokepictures/${pokemon.national_id}.png`} />
+                {pokemon.national_id} -{' '}
+                {pokemon.name}
+              </div>
+            ))}
+          </div>
+          <div class="column is-7">
+            2nd column
+          </div>
+        </div>
       </div>
     );
   }
